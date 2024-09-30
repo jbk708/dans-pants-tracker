3481011aaa5d517c29227c9178fcbf265bf34cda
jbk708 <49291241+jbk708@users.noreply.github.com>
2024-09-30 15:13:45 -0700

    Update getStatus.js

--- netlify/functions/getStatus.js
+++ netlify/functions/getStatus.js
@@ -1,21 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-
 exports.handler = async (event, context) => {
-    const filePath = path.join(__dirname, '..', 'status.json');
+    const status = process.env.STATUS || 'unknown';  // Retrieve the status from the environment variable
     
-    try {
-        const data = fs.readFileSync(filePath, 'utf-8');
-        const jsonData = JSON.parse(data);
-        return {
-            statusCode: 200,
-            body: JSON.stringify(jsonData),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch status' }),
-        };
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ status }),
+    };
 };
-
